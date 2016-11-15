d4f3611651457cb913e57ae9043e4550f70b6d3e
kevintrankt <kevin.tran.kt@gmail.com>
2016-11-14 19:25:30 -0800

    Install

---
+++ script.js
@@ -0,0 +1,19 @@
+function estimateTotal() {
+
+	var itemBball = parseInt(document.getElementById('txt-q-bball').value, 10) || 0;
+	var totalQty = itemBball,
+		totalItemPrice = (1499 * itemBball);
+
+
+	document.getElementById('txt-estimate').value = '$' + totalItemPrice.toFixed(2);
+
+	var results = document.getElementById('results');
+
+	results.innerHTML = 'Total items: ' + totalQty + '<br>';
+}
+
+function checkout() {
+	document.getElementById('order-confirm').innerHTML = 'Order Number: 1';
+}
+
+// JavaScript Document
