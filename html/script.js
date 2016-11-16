817bfc5f50cd223b5c6b5c56cce950b93d7f55ea
kevintrankt <kevin.tran.kt@gmail.com>
2016-11-16 13:08:25 -0800

    Move to HTML folder for webpage

--- html/script.js
+++ html/script.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+
+}
+
+
 function estimateTotal() {
 
 	var itemBball = parseInt(document.getElementById('txt-q-bball').value, 10) || 0;
@@ -15,5 +20,14 @@ function estimateTotal() {
 function checkout() {
 	document.getElementById('order-confirm').innerHTML = 'Order Number: 1';
 }
+
+function setAllProducts(){
+	$("#products").load("products.html");
+}
+
+function setTVs(){
+	$("#products").load("tvs.html");
+}
+
 
 // JavaScript Document
