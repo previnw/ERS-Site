b47d3dea555f39add868a964608223fc22ac2b56
kevintrankt <kevin.tran.kt@gmail.com>
2016-11-30 19:26:37 -0800

    Slkdfaj

--- html/script.js
+++ html/script.js
@@ -35,6 +35,34 @@ function updateAllProducts(){
 	});
 }
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+}
+
+function getCart(){
+	if (!(document.cookie.indexOf('cart') > -1)) {
+			document.cookie = "cart=0;path=/";
+
+	} else {
+		$("#cart-num").html(getCookie("cart"));
+	}
+}
+
 function setAllProducts(){
 	$("#products").load("products.html");
 	$.getJSON('http://ers.kaiw.dk/items/', function(data) {
@@ -43,7 +71,19 @@ function setAllProducts(){
 			$("#item"+j).load("itemTemplate.html #sec-item"+j);
 		}
 	});
-	updateAllProducts();
+
+
+	$.getJSON('http://ers.kaiw.dk/items/', function(data) {
+		x= data.length;
+		for(var j=0;j<x;j++){
+			$("#img-item"+j).attr("src",data[j].photo);
+			$("#txt-item"+j).html(data[j].title);
+			// alert(data[j].photo);
+			var imageElement = document.getElementById('img-item'+j);
+			var anchorElement = imageElement.parentNode;
+			anchorElement.href = 'item.html?item=' + data[j].sku;
+		}
+	});
 }
 
 
